b2ba6188b0065a750a194fca8e6906ba1873458f
zhouweihua <zhouweihua@joyy.com>
2024-07-08 22:36:38 +0800

    feat:格式文件

--- src/transformers/model.transform.ts
+++ src/transformers/model.transform.ts
@@ -1,15 +1,18 @@
-import { Connection as MongodbConnection } from 'mongoose'
-import { Inject, Provider } from '@nestjs/common'
-import { REPOSITORY, DB_CONNECTION_TOKEN } from '@app/constants/system.constant'
-import { getModelForClass } from '@typegoose/typegoose'
+import { Connection as MongodbConnection } from 'mongoose';
+import { Inject, Provider } from '@nestjs/common';
+import {
+  REPOSITORY,
+  DB_CONNECTION_TOKEN,
+} from '@app/constants/system.constant';
+import { getModelForClass } from '@typegoose/typegoose';
 
 export interface TypeClass {
-  new (...args: [])
+  new (...args: []);
 }
 
 // provider名称
 export function getModelName(name: string): string {
-  return name.toLocaleUpperCase() + REPOSITORY
+  return name.toLocaleUpperCase() + REPOSITORY;
 }
 
 // mongodb 工厂提供者
@@ -17,13 +20,15 @@ export function getProviderByTypegoose(typegooseClass: TypeClass): Provider {
   return {
     provide: getModelName(typegooseClass.name),
     useFactory: (connection: MongodbConnection) => {
-      return getModelForClass(typegooseClass, { existingConnection: connection })
+      return getModelForClass(typegooseClass, {
+        existingConnection: connection,
+      });
     },
     inject: [DB_CONNECTION_TOKEN],
-  }
+  };
 }
 
 // Model 注入器
 export function InjectModel(model: TypeClass) {
-  return Inject(getModelName(model.name))
+  return Inject(getModelName(model.name));
 }
