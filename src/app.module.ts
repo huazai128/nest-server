eea6b105e1f14a5ed5d5c35dc14373d6da803712
zhouweihua <zhouweihua@joyy.com>
2024-07-20 14:20:24 +0800

    feat: 优化代码

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import modules from '@app/moduels';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from '@app/processors/database/database.module';
-import modules from '@app/moduels';
 import { MicroserviceModule } from '@app/processors/microservices/microservice.module';
 
 @Module({
