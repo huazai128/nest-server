d61c39b209130ad3b6c99c7e473e6ebfde4d704f
zhouweihua <zhouweihua@joyy.com>
2024-09-04 19:14:34 +0800

    feat: 添加日志

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ async function bootstrap() {
       url: CONFIG.grpcUrl,
       // gRPC 反射
       onLoadPackageDefinition: (pkg, server) => {
+        // nodev22 版本运行这里会报错  ReflectionService。https://github.com/protobufjs/protobuf.js/issues/2025
         // ReflectionService的作用是向gRPC客户端提供有关gRPC服务的元数据信息。它允许客户端查询和发现可用的gRPC服务，以及服务中的方法、消息类型和其他元数据。ReflectionService提供的元数据信息可以用于自动生成客户端代码、动态调用gRPC服务方法以及进行服务发现和探测等。
         new ReflectionService(pkg).addToServer(server);
       },
