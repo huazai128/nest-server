89887dda33cf906ca6847827eb6fdeb04ad36204
zhouweihua <zhouweihua@joyy.com>
2024-11-21 21:46:00 +0800

    feat: log

--- src/moduels/log/log.module.ts
+++ src/moduels/log/log.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common'
-import { LogController } from './log.controller'
-import { LogService } from './log.service'
+import { Module } from '@nestjs/common';
+import { LogController } from './log.controller';
+import { LogService } from './log.service';
 
 @Module({
   imports: [],
